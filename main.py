699cb288bcbcb79d6b02de7ae61f0bdcf6df5855
Dauir <mm0222125@gmail.com>
2023-01-26 22:08:33 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+'''
+if(7 % 2 != 0):
+    print("ODD")
+else:
+    print("EVEN")
+'''
+'''
+x = 8
+X = "heyyy"
+'''
+'''
+print(type(x))
+print(type(X))
+'''
+'''
+print(x)
+print(X)
+'''
+'''
+x, y, z = "apple", "juice", "aaaa"
+print(z)
+print(x)
+print(y)
+'''
+'''
+friuts = ["apple", "juice", "bbbb"]
+x, y, z = friuts
+print(x)
+print(y)
+print(z)
+'''
+'''
+z = "aaaeeeee"
+def myFunc():
+    #global z
+    z = "emes"
+    print("Shara " + z)
+
+def myFunc2():
+        print("Shara " + z)
+myFunc()
+myFunc2()
+'''
+if ( 5 > 2 ):
+ print ("Five is greater than two!")
